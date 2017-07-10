505f4bf810859ff0cf61c424bfc46b109e44a5f7
WS-034-PRO\rcacuango <rcacuango@at4.net>
2017-07-10 18:03:21 +0200

    add Versionig User example gitHub

--- APIdotNet/V1/Models/User.cs
+++ APIdotNet/V1/Models/User.cs
@@ -1,7 +1,16 @@
 ﻿using System;
-namespace APIdotNet.V1.Models
+using Newtonsoft.Json;
+namespace APIdotNet.RoutePrefix
 {
-    public struct User
+    public class User
     {
+       
+        public new string Id { get; set; }
+
+        public virtual string FirstName { get; set; }
+
+        public virtual string LastName { get; set; }
+
+        public virtual string TenantId { get; set; }
     }
 }
--- APIdotNet/V2/Controllers/UserController.cs
+++ APIdotNet/V2/Controllers/UserController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Web.Http;
+
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace APIdotNet.V2.Controllers
 {
+    //[ApiVersion("2.0")]
     [Route("api/[controller]")]
     public class UserController : Controller
     {
